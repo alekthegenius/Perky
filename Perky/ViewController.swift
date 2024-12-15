0a7a903110c7414a8c6f849cabe107b7035595b2
Alek V <alekvasek@icloud.com>
2024-12-14 19:43:30 -0600

    Updated Code

--- Perky/ViewController.swift
+++ Perky/ViewController.swift
@@ -12,7 +12,24 @@ import UserNotifications
 
 class ViewController: NSViewController {
 
-    
+    func optToString(_ opt: Caffeination.Opt) -> String {
+        switch opt {
+        case .idle:
+            return "idle"
+        case .display:
+            return "display"
+        case .disk:
+            return "disk"
+        case .system:
+            return "system"
+        case .user:
+            return "user"
+        case .timed(let seconds):
+            return "timed(\(seconds)s)"
+        case .process(let process):
+            return "process(\(process))"
+        }
+    }
     
     @IBOutlet weak var minuteSlider: NSSlider!
     
@@ -28,7 +45,8 @@ class ViewController: NSViewController {
     
 
     @IBAction func minuteSliderText(_ sender: Any) {
-        minutesLabel.stringValue = minuteSlider.stringValue
+        
+        minutesLabel.stringValue =    String(round(minuteSlider.doubleValue)).replacingOccurrences(of: ".0", with: "") + " Minutes"
     }
     
     var minutesLeft = 0
@@ -37,48 +55,76 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var status: NSTextField!
     
-    @IBOutlet weak var minuteText: NSTextField!
-    
     @IBOutlet weak var caffinateButton: NSButton!
     
-    @IBOutlet weak var devices: NSComboBox!
+    
+    
+    @IBOutlet weak var disk: NSButton!
+    
+    @IBOutlet weak var system: NSButton!
+    
+    @IBOutlet weak var user: NSButton!
+    
+    @IBOutlet weak var display: NSButton!
+    
+    @IBOutlet weak var idle: NSButton!
+    
+    var caf = Caffeination()
+    
+    
+    @IBAction func stopButton(_ sender: NSButton) {
+        caf.stop()
+        
+    }
     
     @IBAction func Caffeniate(_ sender: NSButton) {
         
-        let minutes = minuteSlider.doubleValue
+        let minutes = round(minuteSlider.doubleValue)
         
+//        Create a list that stores the devices selected in the nsbuttons
         
-        switch devices.stringValue {
-        case "All (Idle, Disk, Display, System)":
-            let caf = Caffeination(withOpts: [.idle, .display, .disk, .system, .timed(minutes * 60)])
+        var devicesList: [Caffeination.Opt] = []
             
-            do {
-                try caf.start()
-                status.stringValue =  ("Caffeination has Succesfully Started for \(minutes) minutes and is set to All")
-            } catch {
-                status.stringValue = ("Caffeination failed to start")
-            }
-            
-        case "Keep Only Display Awake(idle, display)":
-            let caf = Caffeination(withOpts: [.idle, .display, .timed(minutes * 60)])
-            do {
-                try caf.start()
-                status.stringValue =  ("Caffeination has Succesfully Started for \(minutes) minutes and is set to Keep Display Awake")
-            } catch {
-                status.stringValue = ("Caffeination failed to start")
-            }
-            
-        default:
-            let caf = Caffeination(withOpts: [.idle, .display, .disk, .system, .timed(minutes * 60)])
-            do {
-                try caf.start()
-                status.stringValue =  ("Caffeination has Succesfully Started for \(minutes) minutes and is set to All")
-            } catch {
-                status.stringValue = ("Caffeination failed to start")
-            }
-            
+        if idle.state == .on {
+            devicesList.append(.idle)
         }
+            
+        if display.state == .on {
+            devicesList.append(.display)
+        }
+            
+        if disk.state == .on {
+            devicesList.append(.disk)
+        }
+            
+        if system.state == .on {
+            devicesList.append(.system)
+        }
+            
+        if user.state == .on {
+            devicesList.append(.user)
+        }
+        
+        if minutes != 0 {
+            devicesList.append(.timed(minutes * 60))
+        }
+        
+        caf = Caffeination(withOpts: devicesList)
+        
+
+        do {
+            try caf.start()
+            let devicesString = devicesList.map { optToString($0) }.joined(separator: ", ")
+            status.stringValue = "Caffeination has Successfully Started for \(String(round(minuteSlider.doubleValue)).replacingOccurrences(of: ".0", with: "")) minutes and is set to [\(devicesString)]"
+        } catch {
+            status.stringValue = "Caffeination failed to start"
+        }
+            
+        
+        
     }
+    
+    
     
     
     
